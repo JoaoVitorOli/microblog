2ae50d6280e9a04b65f9493de3094b6e4ef59b8d
João Vitor <joao.vitor012020@gmail.com>
2026-09-21 19:50:53 -0300

    feat(comments): initialize comments service with Express setup

---
+++ server/comments/src/main.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express();
+
+app.get('/post', (req, res) => {
+
+});
+
+app.post('/post', (req, res) => {
+    
+});
+
+app.listen(4000, () => {
+    console.log('Listening on port 4000.');
+})
