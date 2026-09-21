7c9be788d85ef6c82a8dbd7c53bb31ff946e8d9e
João Vitor <joao.vitor012020@gmail.com>
2026-09-21 20:08:29 -0300

    feat(posts): restructure posts service with improved data handling and formatting

--- server/posts/src/main.ts
+++ server/posts/src/main.ts
@@ -1,15 +1,31 @@
 import express from 'express'
+import { randomBytes } from 'node:crypto'
+import bodyParser from 'body-parser'
+
+interface Posts {
+  title: string;
+}
 
 const app = express();
+app.use(bodyParser.json());
+
+const posts: Record<string, Posts & { id: string }> = {};
 
 app.get('/post', (req, res) => {
-
+  res.send(posts);
 });
 
 app.post('/post', (req, res) => {
-    
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id, title
+  };
+
+  res.status(201).send(posts[id]);
 });
 
 app.listen(4000, () => {
-    console.log('Listening on port 4000.');
+  console.log('Listening on port 4000.');
 })
