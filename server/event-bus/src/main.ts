a3bb2918592bcb0b67ed7bddc55d90c8ba4bfa23
João <joao.vitor012020@gmail.com>
2026-09-23 16:51:31 -0300

    feat(events): implement event storage and retrieval functionality

--- server/event-bus/src/main.ts
+++ server/event-bus/src/main.ts
@@ -5,8 +5,29 @@ import axios from 'axios'
 const app = express();
 app.use(bodyParser.json());
 
+type Status = 'pending' | 'rejected';
+
+interface Comments {
+  id: string;
+  content: string;
+  status: Status;
+}
+
+interface Event {
+  type: string,
+  data: {
+    id: string,
+    title: string,
+    comments: Comments[];
+  }
+}
+
+const events: Event[] = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event).catch((err) => {
     console.log(err.message);
@@ -24,6 +45,9 @@ app.post('/events', (req, res) => {
   res.send({ status: 'OK' });
 });
 
+app.get('/events', (req, res) => {
+  res.send(events);
+})
 
 app.listen(4005, () => {
   console.log('Listening on 4005')
